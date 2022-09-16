4e253b539eb6b20cae8b0dae16339fb772646e85
Yan Damasco <112968787+YanDamasco@users.noreply.github.com>
2022-09-16 17:02:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function clicar() {
+    window.location.href="https://media-exp1.licdn.com/dms/image/C4D03AQHG-YrNolC8hQ/profile-displayphoto-shrink_800_800/0/1663262833005?e=1668643200&v=beta&t=cpAUqTXy4986zXVhKQuAs6ms9pWf_SoJmUUy4N_q7OQ"
+}
+
+function clicar2() {
+    window.open("https://jovemnerd.com.br/", '_blank')
+}
+    
+function voltarTopo() {
+    window.scrollTo(0, 0);
+}
